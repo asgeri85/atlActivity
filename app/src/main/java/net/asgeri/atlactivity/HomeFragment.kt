a5af7a6e22788b12c60792f9ba212543be00c7d1
asgeri85 <iasgeri456@gmail.com>
2024-05-02 14:39:09 +0400

    dialogs add

--- app/src/main/java/net/asgeri/atlactivity/HomeFragment.kt
+++ app/src/main/java/net/asgeri/atlactivity/HomeFragment.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
+import net.asgeri.atlactivity.databinding.CustomDialogBinding
 import net.asgeri.atlactivity.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -28,6 +29,40 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.button.setOnClickListener {
+           createCustomDialog()
+        }
+
+    }
+
+    private fun createDialog() {
+        val dialog = AlertDialog.Builder(requireContext())
+
+        dialog.setTitle("Hata 404")
+        dialog.setMessage("bir hata meydana geldi")
+        dialog.setNegativeButton("Xeyir") { _, _ ->
+            Toast.makeText(context, "Xeyiri basdiniz", Toast.LENGTH_SHORT).show()
+        }
+
+        dialog.setPositiveButton("Beli") { _, _ ->
+            Toast.makeText(context, "Beliye basdiniz", Toast.LENGTH_SHORT).show()
+        }
+
+        dialog.create().show()
+    }
+
+    private fun createCustomDialog() {
+        val dialogBinding = CustomDialogBinding.inflate(layoutInflater)
+
+        val dialog = AlertDialog.Builder(requireContext()).create()
+
+        dialog.setView(dialogBinding.root)
+
+        dialog.show()
+
+        dialogBinding.button3.setOnClickListener {
+            dialog.dismiss()
+        }
 
     }
 
